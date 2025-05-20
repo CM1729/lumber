cced1b841eec626522740d80ef6b3a0a85845567
CM1729 <nuthanv6@gmail.com>
2025-05-20 23:36:31 +0530

    updated app.py

--- app.py
+++ app.py
@@ -210,4 +210,6 @@ def prediction():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+#    app.run(host='0.0.0.0', port=5000,debug=True)
